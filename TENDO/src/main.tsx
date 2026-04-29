f170cbae32b2b9c8036f447920321c722dba2776
Soro63 <Sorolandry7@gmail.com>
2026-04-29 17:50:06 +0000

    suppression dossier TENDO

--- TENDO/src/main.tsx
+++
@@ -1,8 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import "./i18n"; // Import i18n configuration
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
